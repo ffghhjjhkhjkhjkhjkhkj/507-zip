b7d12502689dbc87c82742ea544940ea4ba25cde
507-team <114723388+ffghhjjhkhjkhjkhjkhkj@users.noreply.github.com>
2025-08-19 12:37:47 +0000

    Update start_archiving.py

--- archiving/start_archiving.py
+++ archiving/start_archiving.py
@@ -1,6 +1,26 @@
 import os
+import sys
 
 def archiving(name_archive, namefolderforzip):
     print("Начало упаковки...")
+    if ";" in name_archive:
+        print("ОБНАРУЖЕНА ИНЬЕКЦИЯ КОМАНДЫ!")
+        sys.exit(137)
+
+    if ";" in namefolderforzip:
+        print("ОБНАРУЖЕНА ИНЬЕКЦИЯ КОМАНДЫ!")
+        sys.exit(137)
+
     os.system("zip -r " + name_archive + " " + namefolderforzip)
+    print("УПАКОВКА ЗАВЕРШЕНА!!!!")
+
+def archiving_tar(name_archive, namefolderforzip):
+    print("Начало упаковки...")
+    if ";" in name_archive:
+        print("ОБНАРУЖЕНА ИНЬЕКЦИЯ КОМАНДЫ!")
+        sys.exit(137)
+    if ";" in namefolderforzip:
+        print("ОБНАРУЖЕНА ИНЬЕКЦИЯ КОМАНДЫ!")
+        sys.exit(137)
+    os.system("tar czf " + name_archive + ".tar.gz" " -c " + namefolderforzip)
     print("УПАКОВКА ЗАВЕРШЕНА!!!!")
