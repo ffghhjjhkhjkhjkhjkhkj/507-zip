8ce938b6b879f23f66664f58596a845fa5b773c4
507-team <114723388+ffghhjjhkhjkhjkhjkhkj@users.noreply.github.com>
2025-08-19 12:37:29 +0000

    Update archiving_interface.py

--- archiving/archiving_interface.py
+++ archiving/archiving_interface.py
@@ -1,4 +1,4 @@
-from archiving.start_archiving import archiving
+from archiving.start_archiving import archiving, archiving_tar
 
 def start_of_interface_in_archiving():
     print("Отлично! прошу обратить внимание что это БЕТА функция!")
@@ -7,9 +7,14 @@ def start_of_interface_in_archiving():
     print("Выберите в какой формат архива архивировать файлы:")
     print("")
     print("1: ZIP")
-    print("2: TAR (не работает)")
+    print("2: TAR")
     tiparchiveforzip = input()
     if tiparchiveforzip == "1":
-        print("Отлично напишите название папкии которую упаковать:")
+        print("Отлично напишите название папки которую надо упаковать:")
         namefolderforzip = input()
         archiving(name_archive, namefolderforzip)
+    
+    if tiparchiveforzip == "2":
+        print("Отлично напишите название папки которую надо упаковать")
+        namefolderforzip = input()
+        archiving_tar(name_archive, namefolderforzip)
