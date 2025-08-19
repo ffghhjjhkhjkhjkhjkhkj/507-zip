cb84d87a014c640ef85373d59b21865e102d97cb
507-team <114723388+ffghhjjhkhjkhjkhjkhkj@users.noreply.github.com>
2025-08-19 04:04:12 +0000

    Исходный код обновлен до V1.3

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ resource.setrlimit(resource.RLIMIT_AS, (1_000_000_000, 1_000_000_000))  # 1 ГБ
 def global_exception_handler(exc_type, exc_value, exc_traceback):
          print("Ой. Извините но 507-zip словил неисправимую ошибку!")
          print("Пожалуйста сообщите об этом в issues на гитхабе")
+         e = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
          print("")
-         print("---------------------------------------")
+         print("Возможная проблема:")
+         if "'utf-8' codec can't decode byte" in e:
+            print("Проблема с кодировкой возможно вы ввели НЕ UTF-8 символ")
+         if "EOFError: EOF when reading a line" in e:
+            print("Закрытый терминал")
+         print("")
+         print("---------------------------------------") 
          print("Ошибка:")
          traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stderr)
          print("---------------------------------------")
@@ -27,7 +34,7 @@ choice = input()
 if choice == "1":
     print("Выберите тип архива")
     print("1: ZIP")
-    print("2: TAR (Пока что не работает)")
+    print("2: TAR")
     print("")
     print("(Что бы узнать тип архива посмотрите на конец файла .tar это TAR .zip это ZIP)")
     print("")
@@ -100,7 +107,80 @@ if choice == "1":
         if cozdatpodpapkyforextract == "n":
             print("Окей! папку создавать не буду буду сюда распаковывать")
             print("Распаковываю....")
-            outputofcommandinextractt = os.popen("unzip -o " + namearchiveforextract + " -d " + podpapkaforextract + " 2>&1").read()
+            outputofcommandinextractt = os.popen("unzip -o " + namearchiveforextract + " 2>&1").read()
+            if "bad zipfile offset" in outputofcommandinextractt:
+                print("Ой.... архив поврежден!")
+                print("")
+                print("Лог (для анализа):")
+                print("----------------------------------------------------")
+                print(outputofcommandinextractt)
+                print("----------------------------------------------------")
+                print("")
+                print("Выход")
+                print("Выход. Код: 3")
+                sys.exit(3)
+            print("Распаковка завершена!")
+            print("")
+            print("Удачи!")
+
+    if tiparchiveforextract == "2":
+        print("Напишите название файла архива:")
+        print("ВНИМАНИЕ!: ПОСЛЕ ЭТОГО ЕСЛИ В АРХИВЕ БЫЛИ ФАЙЛЫ КОТОРЫЕ НАЗЫВАЛИСЬ ТАКЖЕ КАК И В ЭТОЙ ДИРЕКТОРИИ ОНИ БУДУТ ПЕРЕЗАПИСАНЫ!")
+        namearchiveforextract = input()
+        print(namearchiveforextract)
+        print("")
+        print("Список файлов в архиве:")
+
+        if  ";" in (repr(namearchiveforextract)):
+            print("СТОП! ОБНАРУЖЕНА ВОЗМОЖНАЯ ПОПЫТКА ИНЬЕКЦИИ КОМАНДЫ!")
+            print("Выход. Код: 137")
+            sys.exit(137)
+
+        if  "&&" in (repr(namearchiveforextract)):
+            print("СТОП! ОБНАРУЖЕНА ВОЗМОЖНАЯ ПОПЫТКА ИНЬЕКЦИИ КОМАНДЫ!")
+            print("Выход. Код: 137")
+            sys.exit(137)
+
+
+        if  "|" in (repr(namearchiveforextract)):
+            print("СТОП! ОБНАРУЖЕНА ВОЗМОЖНАЯ ПОПЫТКА ИНЬЕКЦИИ КОМАНДЫ!")
+            print("Выход. Код: 137")
+            sys.exit(137)
+
+
+        print("------------------------------------------------------")
+        os.system("tar tvf " + namearchiveforextract)
+        print("------------------------------------------------------")
+        print("")
+        if not os.path.exists(namearchiveforextract):
+            print("Стоп а точно ли ты правильно ввел название? а то архива то такого тут нет!")
+            print("Выход. Код: 2")
+            sys.exit(2)
+        print("И последнее... Создать подпапку? Y/N")
+        cozdatpodpapkyforextract = input()
+        podpapkaforextract = namearchiveforextract.replace(".tar", "")
+        
+        if cozdatpodpapkyforextract == "y":
+            print("Создаю...")
+            os.system("mkdir " + podpapkaforextract)
+            print("Папка " + podpapkaforextract + " создана!")
+            print("Распаковываю....")
+            print("Нажмите Ctrl+C для отмены")
+            outputofcommandinextracttwithcozdatpodpapky = os.popen("tar xvf " + namearchiveforextract + " --directory " + podpapkaforextract + " 2>&1").read()
+            print("Распаковка завершена!")
+            print("")
+            print("--------------------------------------------------------")
+            print("Список файлов/каталогов:")
+            os.system("dir " + podpapkaforextract)
+            print("--------------------------------------------------------")
+            print("")
+            print("Удачи!")
+
+
+        if cozdatpodpapkyforextract == "n":
+            print("Окей! папку создавать не буду буду сюда распаковывать")
+            print("Распаковываю....")
+            outputofcommandinextractt = os.popen("tar xvf " + namearchiveforextract + " 2>&1").read()
             if "bad zipfile offset" in outputofcommandinextractt:
                 print("Ой.... архив поврежден!")
                 print("")
